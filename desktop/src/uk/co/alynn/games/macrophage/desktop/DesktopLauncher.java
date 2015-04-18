f6aa45b009258dac710a792a712aba15816e7a76
Alistair Lynn <arplynn@gmail.com>
2015-04-18 16:06:21 +0100

    Initial commit

---
+++ desktop/src/uk/co/alynn/games/macrophage/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package uk.co.alynn.games.macrophage.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import uk.co.alynn.games.macrophage.MacrophageGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MacrophageGame(), config);
+	}
+}
